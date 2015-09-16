3b1c7c46e5e4b64b2ccf592cda4a5d3acacb1bed
Fahim Choudhury <fchoudhury17@kentdenver.org>
2015-09-16 14:34:05 -0600

    adding server.py

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import web
+render = web.template.render('templates/')
 
 urls = (
-'/', 'index'
+	'/', 'index', '/login', 'login'
 )
 
 class index:
 	def GET(self):
-		return "Wow, such web!"
+		return render.index();
 
+class login:
+	def GET(self):
+		return render.login();
 if __name__ == '__main__':
 	app = web.application(urls, globals())
 	app.run()
