d0fb29f1e3bb8eb70027e3fc9fcea4a23db8a27b
Shreyas Gupta <sgupta17@kentdenver.org>
2015-09-17 14:06:54 -0600

    Updated Site

--- static/script.js
+++ static/script.js
@@ -1,13 +1,20 @@
-function login() {
-  var textField = document.getElementById("text-field");
-  var value = textField.value;
-  console.log("You have failed him");
-  window.location = "http://0.0.0.0:8080/"
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    if (h>12) {
+      document.getElementById('txt').innerHTML = (h-12)+":"+m+":"+s;
+    }
+    else {
+      document.getElementById('txt').innerHTML = h+":"+m+":"+s;
+    }
+    var t = setTimeout(function(){startTime()},500);
 }
 
-function changeText(){
-  var textField = document.getElementById("text-field");
-  var value = textField.value;
-  var textToChange = document.getElementById("text-to-change");
-  textToChange.innerHTML = value;
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
